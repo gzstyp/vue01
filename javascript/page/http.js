21e231a011dabc7ea87bee3398dcc683830b8ba6
gzstyp <444141300@qq.com>
2020-05-15 22:12:54 +0800

    工具类.js

---
+++ javascript/page/http.js
@@ -0,0 +1,26 @@
+/**
+ * @作者 田应平
+ * @创建时间 2020-05-15 22:03
+ * @QQ号码 444141300
+ * @官网 http://www.fwtai.com
+*/
+//导入axios
+import axios from 'axios';
+axios.get(url,{
+    params : {
+
+    }
+}).then(result =>{
+
+}).catch(error =>{
+
+});
+
+
+
+
+
+
+
+
+
