22fe1278abd168ff7558d8adf6e4b7d5c191367c
gzstyp <444141300@qq.com>
2020-05-15 23:31:38 +0800

    mvvm

--- javascript/page/mvvm.js
+++ javascript/page/mvvm.js
@@ -8,10 +8,49 @@ class Complier{
     constructor(el,vm){
         //判断el是否是一个元素
         this.el = this.isElementNode(el) ? el : document.querySelector(el);
-        console.info(this.el);
+        var fragment = this.nodeFragment(this.el);
+        //console.info(this.el);
+        //console.info(fragment);
+        //把节点中的内容进行替换
+
+        //最后把替换好的内容塞到页面中
+        this.el.appendChild(fragment);
     }
-    isElementNode(node){
+    isElementNode(node){//判断是不是元素节点
         return node.nodeType === 1;
+    }
+    //把节点移到内存中
+    nodeFragment(node){
+        //创建一个文档碎片
+        var fragment = document.createDocumentFragment();
+        var firstChild;
+        while(firstChild = node.firstChild){
+            fragment.appendChild(firstChild);
+        }
+        return fragment;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 };
 class Vue{
